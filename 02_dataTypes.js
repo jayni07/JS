0602398ebf2453c19be68694e0afbfb09f56508e
jayni_07 <jaynijoshi07@gmail.com>
2024-09-15 18:36:23 +0530

    finished data types

---
+++ 02_dataTypes.js
@@ -0,0 +1,7 @@
+"use strict" //treat all the codes with newer js
+
+//alert(3+3) we are using node.js not browser
+
+console.log("hello"); console.log("hie") //very  bad practice. code should be well formed and structured and readable.
+
+console.log(typeof null);

cfada1f05ab54d0677958f4fbb433a4d2d97ae0e
jayni_07 <jaynijoshi07@gmail.com>
2024-09-16 07:27:54 +0530

    conversion_operations

---
+++ 03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score="33abc"
+
+console.log(typeof score);
+
+let strInNum=Number(score)
+
+
+console.log(typeof strInNum);
+console.log(strInNum);
+
+
+//console.log(Number(score))
+//true->1, false->0
+let isLoggedIn=true;
+let boo=Number(isLoggedIn);
+console.log(boo);
+
+//""=>false
+//"jayni=>true"

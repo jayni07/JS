07159dc15667e8a35c295392dee1c09bfacbd1dc
jayni_07 <jaynijoshi07@gmail.com>
2024-09-16 09:45:24 +0530

    string operations

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -17,3 +17,17 @@ console.log(boo);
 
 //""=>false
 //"jayni=>true"
+
+
+//****************************** STRING OPERATIONS ********************************* */
+//basic operations
+let val=3;
+let negval=-3;
+console.log(negval);
+
+console.log("1"+2);
+console.log("1"+"2");
+console.log("2"+1+1);
+console.log(1+2+"2");
+//if string is fisrst in console log then it will take all inouts as string type as seen in line 30
+//otherwise adds if at last

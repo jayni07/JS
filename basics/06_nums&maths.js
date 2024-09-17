c1e3a7838f35ef0324aa17a2dd690c5ffe60a57b
jayni_07 <jaynijoshi07@gmail.com>
2024-09-17 16:25:37 +0530

    maths&nums

---
+++ basics/06_nums&maths.js
@@ -0,0 +1,23 @@
+const num=900.786//taking it as a number
+
+//console.log(num);
+
+const numb=new Number(800.789) //explicitely giving the value as number
+//console.log(numb)
+
+/*console.log(numb.toString().length);
+console.log(numb.toFixed(3));
+console.log(numb.toPrecision(3));
+*/
+
+//***************Maths**/
+
+console.log(Math.round(4.3));
+console.log(Math.floor(4.3));
+console.log(Math.ceil(4.3));
+console.log(Math.abs(-4));
+
+// min,max
+
+const x=[1,2,4]
+console.log(Math.random(x))
